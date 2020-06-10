9dd616c20e357ac8eb0e8370adfff341b8520179
Eduardo Pereira <eduardo.pereira@digifred.com.br>
2020-06-10 13:10:29 -0300

    Inicializacao da aplicacao com React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyled from './styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyled />
+      <h1>DigiWorks</h1>
+    </div>
+  );
+}
+
+export default App;
